84da3a4866334b1b169042f0eeeae6bb5b4008b1
任贵杰 <812022339@qq.com>
2018-09-11 02:49:25 +0800

    创建项目完成eureka路由

---
+++ src/main/java/com/clsaa/gateway/OpenGatewayApplication.java
@@ -0,0 +1,21 @@
+package com.clsaa.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * <p>
+ * 开放gateway启动类
+ * </p>
+ *
+ * @author 任贵杰 812022339@qq.com
+ * @since 2018-09-11
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OpenGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OpenGatewayApplication.class, args);
+    }
+}
